9abedb90183606845d223a36a17864f785cc1d20
usamasarwar026 <usamasarwar160492@gmail.com>
2025-02-01 13:04:09 +0500

    resolve 404 error

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,34 +7,19 @@ import MagicMushroom from "../../components/magicMushroom/MagicMushroom";
 import Navbar from "../../components/navbar/Navbar";
 import Work from "../../components/work/Work";
 import JoinWork from "../../components/joinWork/JoinWork";
-import { useEffect, useState } from "react";
-import Loader from "../../components/loader/Loader";
 
 export default function Home() {
-  const [loading, SetLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      SetLoading(false);
-    }, 2000);
-  }, []);
   return (
     <div>
-      {loading ? (
-        <Loader />
-      ) : (
-        <>
-          <Navbar />
-          <HeroSection />
-          <Collection />
-          <Browser />
-          <Discover />
-          <MagicMushroom />
-          <Work />
-          <JoinWork />
-          <Footer />
-        </>
-      )}
+      <Navbar />
+      <HeroSection />
+      <Collection />
+      <Browser />
+      <Discover />
+      <MagicMushroom />
+      <Work />
+      <JoinWork />
+      <Footer />
     </div>
   );
 }
